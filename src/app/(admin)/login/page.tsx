495c4eacba14016f4fbe629b00dcd6bff00ed3e4
hyroxseouldev <confucious12@naver.com>
2025-08-27 14:12:24 +0900

    로그인 폼 추가

---
+++ src/app/(admin)/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-from";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/login-from.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn("flex flex-col gap-6 w-full max-w-md", className)}
+      {...props}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>관리자 로그인</CardTitle>
+          <CardDescription>관리자 로그인 페이지입니다.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">이메일</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">비밀번호</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  로그인
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
