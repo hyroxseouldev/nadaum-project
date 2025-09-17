9347eac3a0d44cc367ec7079f513aed0cf656a55
hyroxseouldev <confucious12@naver.com>
2025-09-17 17:10:46 +0900

    Add Open Graph and Twitter metadata to layout.tsx for enhanced social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,27 @@ const notoSansMono = Noto_Sans_Mono({
 export const metadata: Metadata = {
   title: "나다움 프로젝트",
   description: "나다움, 우리안의 이야기",
+  openGraph: {
+    title: "나다움 프로젝트",
+    description: "나다움, 우리안의 이야기",
+    images: [
+      {
+        url: "/logo/logo.png", // public 폴더에 있는 이미지
+        width: 1200,
+        height: 630,
+        alt: "나다움 프로젝트 미리보기",
+      },
+    ],
+    type: "website",
+    locale: "ko_KR",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "나다움 프로젝트",
+    description: "나다움, 우리안의 이야기",
+    images: ["/logo/logo.png"],
+  },
 };
-
 export default function RootLayout({
   children,
   modal,
